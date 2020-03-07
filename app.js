a672967e09636df2d16e578cfd7b38eb83961dca
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-03-07 22:00:53 +0100

    Conectada base de datos

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
+
+require('dotenv').config();
 
 var app = express();
+
+//aqui es donde conectamos con la base de datos
+
+require('./db').connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
