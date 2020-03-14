1ccc4bd6edff88594f74dc028543568398d55e25
angelfullstack <angelfullstack@gmail.com>
2020-03-14 11:38:44 +0100

    Parseados datos en create (Post Model) para que lleguen correctamente a my SQL

--- models/post.js
+++ models/post.js
@@ -1,41 +1,81 @@
 const getAll = () => {
-    return new Promise((resolve, reject) => {
-        db.query('select * from posts', (err, rows) => {
-            if (err) reject(err)
-            resolve(rows);
-        });
+  return new Promise((resolve, reject) => {
+    db.query("select * from posts", (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
     });
+  });
 };
 
-const getById = (pPostId) => {
-    return new Promise((resolve, reject) => {
-        db.query('select * from posts where id = ?', [pPostId], (err, rows) => {
-            if(err) reject(err);
-            if(rows.lenght === 0) {
-                resolve(null);
-            }
-            resolve(rows[0]);
-        })
+const getById = pPostId => {
+  return new Promise((resolve, reject) => {
+    db.query("select * from posts where id = ?", [pPostId], (err, rows) => {
+      if (err) reject(err);
+      if (rows.lenght === 0) {
+        resolve(null);
+      }
+      resolve(rows[0]);
     });
+  });
 };
 
-const create = ({titulo, contenido, imagen, publico, colaborable, latitud, longitud, fk_id_anterior, fk_usuario}) => {
-    return new Promise((resolve, reject) => {
-        let numero_autores=null;
-        if(fk_id_anterior===null){
-            numero_autores=1;
-        }
-        db.query('INSERT INTO posts (titulo, contenido, imagen, numero_autores, publico, colaborable, latitud, longitud, fecha_publicacion, fk_id_anterior, fk_usuario) values (?,?,?,?,?,?,?,?,?,?,?)',
-         [titulo, contenido, imagen, numero_autores, publico, colaborable, latitud, longitud, new Date(), fk_id_anterior, fk_usuario],
-         (err, result) => {
-             if (err) reject(err);
-             resolve(result);
-         })
-    });
-}
+const create = ({
+  titulo,
+  contenido,
+  imagen,
+  publico,
+  colaborable,
+  latitud,
+  longitud,
+  fk_id_anterior,
+  fk_usuario
+}) => {
+  return new Promise((resolve, reject) => {
+    //Partimos de poner el número de autores en null como si todos los posts fueran intermedios
+    let numero_autores = null;
+    //Como todavía no sabemos como vamos a recibir los datos del front, comprobamos las cuatro posibilidades del valor fk_id_anterior. Además nos aseguramos que aunque no lleguen bien los datos,éstos se acomoden siempre al modelo de la tabla.
+    if (
+      fk_id_anterior == 0 ||
+      fk_id_anterior === 0 ||
+      fk_id_anterior === null ||
+      fk_id_anterior === undefined
+    ) {
+      //si se cumple lo anterior, es decir, es un post inicial, le damos el primer valor a numero_autores y aseguramos que fk_id_anterior sea null. Puede que más adelante la segunda sentencia no sea necesaria
+      numero_autores = 1;
+      fk_id_anterior = null;
+    }
+
+    //Para los valores latitud, longitud e imagen hay que hacer algo parecido, para que en SQL se graben como null y no como 0. Los pongo en ternario, que lo permiten y es más cómodo:
+
+    imagen = imagen === "" || imagen === undefined ? null : imagen;
+    latitud = latitud == 0 || latitud == undefined ? null : latitud;
+    longitud = longitud == 0 || longitud == undefined ? null : longitud;
+
+    db.query(
+      "INSERT INTO posts (titulo, contenido, imagen, numero_autores, publico, colaborable, latitud, longitud, fecha_publicacion, fk_id_anterior, fk_usuario) values (?,?,?,?,?,?,?,?,?,?,?)",
+      [
+        titulo,
+        contenido,
+        imagen,
+        numero_autores,
+        publico,
+        colaborable,
+        latitud,
+        longitud,
+        new Date(),
+        fk_id_anterior,
+        fk_usuario
+      ],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
 
 module.exports = {
-    getAll: getAll,
-    getById: getById,
-    create: create
-}
+  getAll: getAll,
+  getById: getById,
+  create: create
+};
